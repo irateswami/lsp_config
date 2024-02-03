acadded5eb8a5cd09e7cfbdea12fcebc38409d0c
Bryan English <40923362+irateswami@users.noreply.github.com>
2024-02-02 18:34:22 -0600

    lots of fun new things

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,69 +1,86 @@
 vim.api.nvim_create_augroup("AutoFormat", {})
-vim.api.nvim_create_autocmd(
-    "BufWritePre",
-    {
-        pattern = "*.go",
-        group = "AutoFormat",
-        callback = function ()
-            vim.cmd("%!gofumpt")
-            vim.cmd("write!")
-        end,
-    }
-)
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        local cursor_position = vim.api.nvim_win_get_cursor(0)
+        vim.cmd("%!gofumpt")
+        vim.api.nvim_win_set_cursor(0, cursor_position)
+    end
+})
 
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "gc", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gc", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {'tsserver', 'rust_analyzer', 'gopls'},
-	handlers = {
-		lsp_zero.default_setup,
-		lua_ls = function()
-			local lua_opts = lsp_zero.nvim_lua_ls()
-			require('lspconfig').lua_ls.setup(lua_opts)
-		end,
-	}
+    ensure_installed = { 'tsserver', 'rust_analyzer', 'gopls' },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    }
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local lspkind = require('lspkind')
 
 cmp.setup({
-	sources = {
-		{name = 'path'},
-		{name = 'nvim_lsp'},
-		{name = 'nvim_lua'},
-		{name = 'luasnip', keyword_length = 2},
-		{name = 'buffer', keyword_length = 3},
-	},
-	format = lspkind.cmp_format({
-		mode = 'symbol', -- show only symbol annotations
-		maxwidth = 50, -- prevent the popup from showing more than provided characters
-		ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-		show_labelDetails = true,
-	}),
-	mapping = cmp.mapping.preset.insert({
-		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		['<C-y>'] = cmp.mapping.confirm({ select = true }),
-		['<C-Space>'] = cmp.mapping.complete(),
-	}),
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip', keyword_length = 2 },
+        { name = 'buffer',  keyword_length = 3 },
+    },
+    format = lspkind.cmp_format({
+        mode = 'symbol',       -- show only symbol annotations
+        maxwidth = 50,         -- prevent the popup from showing more than provided characters
+        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+        show_labelDetails = true,
+    }),
+    mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
 })
 
+local copy_symbol_under_cursor = function()
+    -- Get the word under the cursor as a fallback
+    local symbol = vim.fn.expand('<cword>')
+    -- Copy the symbol to the clipboard
+    vim.fn.setreg('+', symbol)
+end
+
+vim.api.nvim_create_user_command('CopySymbolName', copy_symbol_under_cursor, {})
+
+vim.api.nvim_set_keymap('n', '<leader>cs', ':CopySymbolName<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>cp', ':let @+=expand("%:p")<CR>', { noremap = true, silent = true })
+
+local function copy_highlight_to_clipboard()
+  vim.cmd('normal! y')
+  vim.notify("Highlighted text copied to clipboard")
+end
+
+-- Map this function to a visual mode keybinding
+vim.api.nvim_set_keymap('v', '<leader>y', ':lua copy_highlight_to_clipboard()<CR>', { noremap = true, silent = true })
--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
 require("irateswami")
+
+vim.api.nvim_set_keymap('n', '<leader>cp', ':let @+=expand("%:p")<CR>', { noremap = true, silent = true })
+
