0ccc824890aa043a44e5c65f7f9417bc3522e0a4
Bryan English <40923362+irateswami@users.noreply.github.com>
2024-04-16 14:40:48 -0500

    modified a couple things

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,12 +1,20 @@
 vim.api.nvim_create_augroup("AutoFormat", {})
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-        local cursor_position = vim.api.nvim_win_get_cursor(0)
-        vim.cmd("silent %!gofumpt | goimports")
-        vim.api.nvim_win_set_cursor(0, cursor_position)
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
     end
+    vim.lsp.buf.format({async = false})
+  end
 })
 
 local lsp_zero = require('lsp-zero')
@@ -64,6 +72,19 @@ cmp.setup({
 })
 
 local lspconfig = require('lspconfig')
+
+lspconfig.gopls.setup({
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+      --goimports = true,
+    },
+  },
+})
 
 lspconfig.htmx.setup{}
 
